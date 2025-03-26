9a690aaa9c8fdfdcc5716339d28c6e31272ab770
Huigyeong Na <nhk1007@naver.com>
2025-03-26 20:56:30 +0900

    로그인 구현 완. 모바일에서 포스트잇 크기 조절 필요할듯? 복지몰 메인 페이지 구현하고 로그인 시 넘어가는 거 구현해야함요

---
+++ JS/login.js
@@ -0,0 +1,35 @@
+const correctID = 'yongj1111';
+const correctPW = 'Dydajflgodks!111';
+const errorPW = 'dydajflgodks!111';
+
+let enteredIDElement = document.getElementById('userID');
+let enteredPWElement = document.getElementById('userPW');
+let loginErrorElement = document.getElementById('login-error');
+
+let loginBtn = document.getElementById('login-btn');
+loginBtn.addEventListener('click', checkLogin);
+
+enteredPWElement.addEventListener('keydown', enterLogin);
+
+function enterLogin(event){
+    if(event.key == 'Enter'){
+        checkLogin();
+    }
+}
+
+function checkLogin(){
+    let enteredID = enteredIDElement.value;
+    let enteredPW = enteredPWElement.value;
+
+    if(enteredID == correctID && enteredPW == correctPW){
+        alert('환영합니다. 이자헌 과장님.');
+    }
+    else if(enteredID == correctID && enteredPW == errorPW){
+        loginErrorElement.style.display = 'block';
+        alert('어디 보자. 회원가입을 누르면 비밀번호 설정 요구사항을 확인할 수 있다.\n\'대문자가 필요했군.\'')
+    }
+    else{
+        loginErrorElement.style.display = 'block';
+        alert('역시나 포스트잇이 붙어 있었다.');
+    }
+}
